cadcc1265b083206ce454a31d4574daa2c9f5451
Matija Petrovic <matijapetrovicweb@gmail.com>
2020-05-03 12:27:51 +0200

    Attempting moving security to adapters

---
+++ medihub-api/medihub-application/src/main/java/org/medihub/application/ports/incoming/authentication/LoginUseCase.java
@@ -0,0 +1,30 @@
+package org.medihub.application.ports.incoming.authentication;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.medihub.common.SelfValidating;
+import org.medihub.domain.identity.Account;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public interface LoginUseCase {
+    Account login(LoginCommand command);
+
+    @Value
+    @EqualsAndHashCode(callSuper = false)
+    class LoginCommand extends SelfValidating<LoginCommand> {
+        @Email
+        String email;
+        @NotBlank
+        String password;
+
+        public LoginCommand(
+                String email,
+                String password) {
+            this.email = email;
+            this.password = password;
+            this.validateSelf();
+        }
+    }
+}
---
+++ medihub-api/medihub-infrastructure/src/main/java/org/medihub/security/identity/CustomGrantedAuthority.java
@@ -0,0 +1,15 @@
+package org.medihub.security.identity;
+
+import lombok.RequiredArgsConstructor;
+import org.medihub.domain.identity.Authority;
+import org.springframework.security.core.GrantedAuthority;
+
+@RequiredArgsConstructor
+public class CustomGrantedAuthority implements GrantedAuthority {
+    private final Authority authority;
+
+    @Override
+    public String getAuthority() {
+        return authority.getName();
+    }
+}
