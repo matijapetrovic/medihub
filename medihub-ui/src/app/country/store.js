c93ce90bded4c48d2783d52a1fc91e1effc710e4
bodroza.joca <bodroza.joca@gmail.com>
2020-05-09 13:12:33 +0200

    Merging

--- medihub-ui/src/app/country/store.js
+++ medihub-ui/src/app/country/store.js
@@ -19,7 +19,7 @@ export default {
         })
         .catch((err) => {
           dispatch('notifications/add', utils.errorNotification(err), { root: true });
-        })
+        });
     },
   },
 };
