c34a20a07b466cb72870ff5285ba1b245e4341d5
nikolakabasaj <44526113+nikolakabasaj@users.noreply.github.com>
2020-05-25 22:09:04 +0200

    Adapted the way it is added to the database

--- medihub-api/medihub-infrastructure/src/main/java/org/medihub/persistence/medical_doctor_schedule/MedicalDoctorVacationScheduleItemRepository.java
+++ medihub-api/medihub-infrastructure/src/main/java/org/medihub/persistence/medical_doctor_schedule/MedicalDoctorVacationScheduleItemRepository.java
@@ -2,9 +2,9 @@ package org.medihub.persistence.medical_doctor_schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.Time;
+import java.sql.Date;
 import java.util.Optional;
 
 public interface MedicalDoctorVacationScheduleItemRepository extends JpaRepository<MedicalDoctorVacationScheduleJpaItem, Long> {
-    Optional<MedicalDoctorVacationScheduleJpaItem> findByTimeAndAndScheduleId(Time time, Long ScheduleId);
+    Optional<MedicalDoctorVacationScheduleJpaItem> findByAndEndDateAndAndScheduleId(Date date, Long ScheduleId);
 }
