e96657e97f73c386cb80abc817ccbb1ddb87f63e
Matija Petrovic <matijapetrovicweb@gmail.com>
2020-06-07 14:22:20 +0200

    Reverted doctor table change

--- medihub-ui/src/app/doctor/_components/DoctorSearchTable.vue
+++ medihub-ui/src/app/doctor/_components/DoctorSearchTable.vue
@@ -1,52 +1,36 @@
 <template>
-  <div>
-    <v-data-table
-      :headers="headers"
-      :items="items"
-      :items-per-page="5"
-      @item-expanded="getAvailableTimes"
-      item-key="id"
-      show-expand
-      single-expand
-      class="elevation-1"
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="5"
+    @item-expanded="getAvailableTimes"
+    item-key="id"
+    show-expand
+    single-expand
+    class="elevation-1"
+  >
+  <template
+    v-slot:expanded-item="{ headers, item }">
+    <td
+      :colspan="headers.length"
     >
-    <template
-      v-slot:expanded-item="{ headers, item }">
-      <td
-        :colspan="headers.length"
+      <tr
+        v-for="time in item.availableTimes"
+        :key="time"
       >
-        <tr
-          v-for="time in item.availableTimes"
-          :key="time"
-        >
-          <td>{{ time }}</td>
-          <td><v-btn @click="schedule(item.id, time)">Schedule Appointment</v-btn></td>
-        </tr>
-      </td>
-    </template>
-    </v-data-table>
-    <v-dialog
-      v-model="dialog"
-      max-width="500"
-    >
-      <ScheduleAppointmentDialog
-        :appointment="appointment"
-        @cancelled="closeConfirmScheduleDialog"
-        @scheduled="schedule"
-      />
-    </v-dialog>
-  </div>
+        <td>{{ time }}</td>
+        <td><v-btn @click="schedule(item.id, time)">Schedule Appointment</v-btn></td>
+      </tr>
+    </td>
+  </template>
+  </v-data-table>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex';
-import ScheduleAppointmentDialog from '@/app/appointment/_components/ScheduleAppointmentDialog.vue';
 
 export default {
   name: 'DoctorSearchTable',
-  components: {
-    ScheduleAppointmentDialog,
-  },
   data: () => ({
     headers: [
       {
